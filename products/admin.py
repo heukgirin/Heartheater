5d99b2dab7772ff9823792c20a138345f7ecd0c8
heukgirin <heukgirin@naver.com>
2026-02-07 15:28:36 +0900

    Delete products directory

--- products/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Attr, Cont
-
-admin.site.register(Attr)
-admin.site.register(Cont)
--- products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import List, WriteCont, WriteAttr
-
-urlpatterns = [
-    path('', List.as_view()),
-    path('list', List.as_view()),
-    path('write/cont/', WriteCont.as_view()),
-    path('write/attr/', WriteAttr.as_view()),
-]
